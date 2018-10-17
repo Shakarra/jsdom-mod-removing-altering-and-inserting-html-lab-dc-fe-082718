917f04784fc0b1c49527753deac339666c528b14
Shakarra McGuire <shakarramcguire@gmail.com>
2018-10-17 23:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+=======
+main.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.HTML = "Shakarra is the champpion!";
+body.appendChild(newHeader);
+          
+>>>>>>> f1e78ec00a55c8d4b61b783c79e2706f882998f9
